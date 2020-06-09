8b103fc21f6652ed792fb8082fcc16f4d8ec266e
EliezerTeixeira <eliezerteixeira@outlook.pt>
2020-06-09 14:18:33 +0100

    added project

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+// Iniciando o App
+const server = express()
+server.use(express.json())
+server.use(cors())
+
+// Conectando com DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true, useUnifiedTopology: true})
+require('./src/models/Product')
+
+//Rotas
+server.use('/api', require('./src/routes'))
+
+server.listen(3000)
